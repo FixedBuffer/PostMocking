fe679290804546ff120f4cdb70d4a662e866fc4b
Jorge Turrado Ferrero <jturrado@stt-systems.com>
2018-10-10 21:00:11 +0200

    Añado referencias y paquetes para el proyecto de pruebas unitarias

---
+++ PruebasUnitarias/GeneradorInformesTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PruebasUnitarias
+{
+    [TestClass]
+    public class GeneradorInformesTests
+    {
+        [TestMethod]
+        public void GenerarInforme()
+        {
+        }
+    }
+}
