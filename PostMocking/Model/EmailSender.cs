830d68caa4b6f06cfbf08ad7ce588a4d3d471974
Jorge Turrado Ferrero <jturrado@stt-systems.com>
2018-10-10 20:52:05 +0200

    Añado la clase que implementa IEmailSender

---
+++ PostMocking/Model/EmailSender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PostMocking.Model
+{
+    public class EmailSender : IEmailSender
+    {
+        public bool Enviar(string Destinatario, string Mensaje)
+        {
+            return true;
+        }
+    }
+}
