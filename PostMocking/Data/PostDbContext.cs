6ca5bfb1c048823da9aeae934b59fa9ad82b40b5
Jorge Turrado Ferrero <jturrado@stt-systems.com>
2018-10-10 20:23:12 +0200

    cambio nombre de DB

--- PostMocking/Data/PostDbContext.cs
+++ PostMocking/Data/PostDbContext.cs
@@ -24,7 +24,7 @@ namespace PostMocking.Data
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseMySql("Server=localhost;Database=postefcore;Uid=root;Pwd=root;");
+                optionsBuilder.UseMySql("Server=localhost;Database=PostMocking;Uid=root;Pwd=root;");
             }
         }
     }
